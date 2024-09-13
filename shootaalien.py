f2e56f0262e035760dcd1dd08696c87fa0b4254d
Julian-Friday <fridayjulian13@gmail.com>
2024-09-13 18:02:52 +0200

    Add files via upload

---
+++ shootaalien.py
@@ -0,0 +1,24 @@
+import pgzrun
+HEIGHT=600
+WIDTH=700
+TITLE="Shootaalien"
+moonman=Actor("moonman")
+def draw():
+    screen.clear()
+    screen.fill("black")
+    moonman.draw()
+    
+
+
+
+
+
+
+
+
+
+
+
+def update():
+    pass
+pgzrun.go()
