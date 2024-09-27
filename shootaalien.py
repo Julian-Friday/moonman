4fc5c76ea0bcaf19856d47115a3ecf6e6671842f
Julian-Friday <fridayjulian13@gmail.com>
2024-09-27 18:03:28 +0200

    Add files via upload

--- shootaalien.py
+++ shootaalien.py
@@ -1,15 +1,33 @@
 import pgzrun
+import random
 HEIGHT=600
 WIDTH=700
 TITLE="Shootaalien"
 moonman=Actor("moonman")
+counter=0
 def draw():
     screen.clear()
     screen.fill("black")
+    screen.draw.text("Aliens hit:"+str(counter),(300,250))
     moonman.draw()
+def on_mouse_down(pos):
+    global counter
+    if moonman.collidepoint(pos):
+        x=random.randint(0,700)
+        y=random.randint(0,600)
+        moonman.pos=(x,y)
+        counter=counter+1
+        
+
+
+
     
 
 
+
+    
+    
+        
 
 
 
